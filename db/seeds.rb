50dd9a25bcf3095256d1a9e5ee1f05eb5922f6d5
andreamazzatxt <andreamazza89@gmail.com>
2021-03-02 15:32:47 +0100

    Add basic styling

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+10.times do
+  title = Faker::TvShows::BojackHorseman.tongue_twister
+  content = Faker::TvShows::BojackHorseman.quote
+  Article.create(title: title, content: content)
+end
